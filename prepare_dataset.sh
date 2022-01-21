95749fa6f0f0810a7024d85f5d117de7d628ad50
HiroIshida <spitfire.docomo@gmail.com>
2022-01-21 22:16:02 +0900

    Add git submodule and create symlinks

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
-mkdir models
-wget https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth -O models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth
+
+ln -sf Detic/configs detic_configs 
+
+cd node_script
+ln -sf ../Detic/detic
+ln -sf ../Detic/third_party
+cd ..
+
+if [ ! -d "models" ]; then
+    mkdir models
+    wget https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth -O models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth
+fi
