e47953289c3af2dd4739040b1dfe86afac6f1eeb
HiroIshida <spitfire.docomo@gmail.com>
2022-01-22 01:54:24 +0900

    Temp proto python node

--- node_script/node.py
+++ node_script/node.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 import argparse
+import dataclasses
 import glob
 import multiprocessing as mp
 import numpy as np
@@ -16,6 +18,8 @@ from detectron2.utils.logger import setup_logger
 
 import rospy
 import rospkg
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
 
 sys.path.insert(0, os.path.join(sys.path[0], 'third_party/CenterNet2/projects/CenterNet2/'))
 
@@ -27,22 +31,23 @@ from detic.predictor import VisualizationDemo
 def cfg_from_rosparam():
     """ 
     setup detctron2 config
-    Similar to https://github.com/facebookresearch/Detic/demo.py
     """
 
     rospack = rospkg.RosPack()
+    pack_path = rospack.get_path('detic_ros')
     default_detic_config_path = os.path.join(
-        rospack.get_path('detic_ros'), 'detic_configs', 
+        pack_path, 'detic_configs', 
         'Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.yaml')
 
     default_model_weights_path = os.path.join(
-        rospack.get_path('detic_ros'), 'models',
+        pack_path, 'models',
         'Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth')
 
 
     detic_config_path = rospy.get_param('~detic_config_path', default_detic_config_path)
     confidence_threshold = rospy.get_param('~confidence_threshold', 0.5)
     model_weights_path = rospy.get_param('~model_weights_path', default_model_weights_path)
+
 
     cfg = get_cfg()
     add_centernet_config(cfg)
@@ -52,7 +57,43 @@ def cfg_from_rosparam():
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = confidence_threshold
     cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = confidence_threshold
     cfg.merge_from_list(['MODEL.WEIGHTS', model_weights_path])
+
+    # Similar to https://github.com/facebookresearch/Detic/demo.py
+    cfg.MODEL.ROI_BOX_HEAD.ZEROSHOT_WEIGHT_PATH = 'rand' # load later
+    cfg.MODEL.ROI_HEADS.ONE_CLASS_PER_PROPOSAL = True
+
+    # Maybe should edit detic_configs/Base-C2_L_R5021k_640b64_4x.yaml
+    cfg.MODEL.ROI_BOX_HEAD.CAT_FREQ_PATH = os.path.join(
+            pack_path, 'datasets/metadata/lvis_v1_train_cat_info.json')
+
+    cfg.freeze()
     return cfg
 
 if __name__=='__main__':
     cfg = cfg_from_rosparam()
+
+
+    @dataclasses.dataclass
+    class DummyArgs: 
+        # Because VisualizationDemo class requires args
+        vocabulary: str
+        @classmethod
+        def from_rosparam(cls): 
+            vocabulary = rospy.get_param('~vocabulary', 'lvis')
+            assert vocabulary in ['lvis', 'openimages', 'objects365', 'coco', 'custom']
+            return cls(vocabulary)
+
+
+    mp.set_start_method("spawn", force=True)
+    # TODO(HiroIshida) add logger ??
+    demo = VisualizationDemo(cfg, DummyArgs.from_rosparam())
+
+
+    rospy.init_node('detic_node', anonymous=True)
+    def callback(msg):
+        bridge = CvBridge()
+        img = bridge.imgmsg_to_cv2(self._image_msg, desired_encoding='passthrough')
+        predictions, visualized_output = demo.run_on_image(img)
+
+    input_image = rospy.get_param('~input_image', 'image')
+    rospy.Subscriber(input_image, Image, callback)
--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
 ln -sf Detic/configs detic_configs 
+ln -sf Detic/datasets datasets
 
 cd node_script
 ln -sf ../Detic/detic
 ln -sf ../Detic/third_party
+ln -sf ../Detic/dataset
 cd ..
 
 if [ ! -d "models" ]; then
