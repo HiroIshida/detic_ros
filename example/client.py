04603153ee641e1571c8dcea80ebe0b1e7eb856c
HiroIshida <spitfire.docomo@gmail.com>
2022-04-06 02:34:00 +0900

    Enable loading custom image

--- example/client.py
+++ example/client.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
-import numpy as np
+import argparse
+import cv_bridge
+import cv2
 import rospy
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from detic_ros.srv import DeticSeg
+import matplotlib.pyplot as plt
 
-def numpy_to_imgmsg(data: np.ndarray) -> Image:
-    # see: cv_bridge/core.py
-    img_msg = Image()
-    img_msg.height = data.shape[0]
-    img_msg.width = data.shape[1]
-    img_msg.encoding = 'bgr8'
-    img_msg.data = data.tostring()
-    img_msg.step = len(img_msg.data) // img_msg.height
-    img_msg.is_bigendian = True
-    return img_msg
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-image', type=str, help='input image path')
+    args = parser.parse_args()
+    image = cv2.imread(args.image)
+    assert image is not None
 
-rospy.init_node('example_client')
-rospy.wait_for_service('/docker/detic_segmentor/segment_image')
-arr = np.random.randint(0, high=255, size=(100, 100, 3), dtype=np.uint8)
-image_msg = numpy_to_imgmsg(arr)
+    rospy.init_node('example_client')
+    rospy.wait_for_service('/docker/detic_segmentor/segment_image')
+    image_msg = CvBridge().cv2_to_imgmsg(image, encoding='passthrough')
 
-f = rospy.ServiceProxy('/docker/detic_segmentor/segment_image', DeticSeg)
-resp = f(image_msg)
-seg_info = resp.seg_info
+    f = rospy.ServiceProxy('/docker/detic_segmentor/segment_image', DeticSeg)
+    resp = f(image_msg)
+    seg_info = resp.seg_info
+    if resp.debug_image is not None:
+        image = CvBridge().imgmsg_to_cv2(resp.debug_image)
+        plt.imshow(image)
+        plt.show()
