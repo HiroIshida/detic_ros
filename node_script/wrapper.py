de85054e682766e448c4f0183a9e55ee591cb0fe
HiroIshida <spitfire.docomo@gmail.com>
2022-11-12 21:08:25 +0900

    refactor: wrapper should not have internal state

--- node_script/wrapper.py
+++ node_script/wrapper.py
@@ -1,5 +1,6 @@
 import os
-from typing import List, Optional, Tuple
+from dataclasses import dataclass
+from typing import List, Optional
 
 import detic
 import numpy as np
@@ -15,6 +16,56 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Header
 
 from detic_ros.msg import SegmentationInfo
+
+
+@dataclass(frozen=True)
+class InferenceRawResult:
+    segmentation_raw_image: np.ndarray
+    class_indices: List[int]
+    scores: List[float]
+    visualization: Optional[VisImage]
+    header: Header
+    class_names: List[str]
+
+    def get_ros_segmentaion_image(self) -> Image:
+        seg_img = CvBridge().cv2_to_imgmsg(self.segmentation_raw_image, encoding="32SC1")
+        seg_img.header = self.header
+        return seg_img
+
+    def get_ros_debug_image(self) -> Image:
+        message = "you didn't configure the wrapper so that it computes the debug images"
+        assert self.visualization is not None, message
+        debug_img = CvBridge().cv2_to_imgmsg(
+            self.visualization.get_image(), encoding="rgb8")
+        debug_img.header = self.header
+        return debug_img
+
+    def get_ros_debug_segmentation_img(self) -> Image:
+        # Call after inference_step
+        human_friendly_scaling = 255 // self.segmentation_raw_image.max()
+        new_data = (self.segmentation_raw_image * human_friendly_scaling).astype(np.uint8)
+        debug_seg_img = CvBridge().cv2_to_imgmsg(new_data, encoding="mono8")
+        assert self.header is not None
+        debug_seg_img.header = self.header
+        return debug_seg_img
+
+    def get_label_array(self) -> LabelArray:
+        labels = [Label(id=i + 1, name=self.class_names[i]) for i in self.class_indices]
+        lab_arr = LabelArray(header=self.header, labels=labels)
+        return lab_arr
+
+    def get_score_array(self) -> VectorArray:
+        vec_arr = VectorArray(header=self.header, vector_dim=len(self.scores), data=self.scores)
+        return vec_arr
+
+    def get_segmentation_info(self) -> SegmentationInfo:
+        seg_img = self.get_ros_segmentaion_image()
+        detected_classes_names = [self.class_names[i] for i in self.class_indices]
+        seg_info = SegmentationInfo(detected_classes=detected_classes_names,
+                                    scores=self.scores,
+                                    segmentation=seg_img,
+                                    header=self.header)
+        return seg_info
 
 
 class DeticWrapper:
@@ -42,8 +93,6 @@ class DeticWrapper:
         self.node_config = node_config
         self.bridge = CvBridge()
         self.class_names = self.predictor.metadata.get("thing_classes", None)
-        self.header = None
-        self.data = None
 
     @staticmethod
     def _adhoc_hack_metadata_path():
@@ -54,10 +103,9 @@ class DeticWrapper:
         for key in path_dict.keys():
             path_dict[key] = os.path.join(pack_path, path_dict[key])
 
-    def inference_step(self, msg: Image) -> Tuple[Image, List[int], List[float], Optional[VisImage]]:
+    def inference_step(self, msg: Image) -> InferenceRawResult:
         # Segmentation image, detected classes, detection scores, visualization image
         img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-        self.header = msg.header
 
         if self.node_config.verbose:
             time_start = rospy.Time.now()
@@ -83,68 +131,15 @@ class DeticWrapper:
             mask = instances.pred_masks[i]
             # label 0 is reserved for background label, so starting from 1
             data[mask] = (i + 1)
-        self.data = data
-        seg_img = self.bridge.cv2_to_imgmsg(data, encoding="32SC1")
-        seg_img.header = self.header
 
         # Get class and score arrays
         class_indexes = instances.pred_classes.tolist()
         scores = instances.scores.tolist()
-        return seg_img, class_indexes, scores, visualized_output
-
-    def get_debug_img(self, visualized_output: VisImage) -> Image:
-        # Call after inference_step
-        debug_img = self.bridge.cv2_to_imgmsg(visualized_output.get_image(),
-                                              encoding="rgb8")
-        assert self.header is not None
-        debug_img.header = self.header
-        return debug_img
-
-    def get_debug_segimg(self) -> Image:
-        # Call after inference_step
-        assert self.data is not None
-        human_friendly_scaling = 255 // self.data.max()
-        new_data = (self.data * human_friendly_scaling).astype(np.uint8)
-        debug_seg_img = self.bridge.cv2_to_imgmsg(new_data, encoding="mono8")
-        assert self.header is not None
-        debug_seg_img.header = self.header
-        return debug_seg_img
-
-    def get_segmentation_info(self, seg_img: Image,
-                              detected_classes: List[int],
-                              scores: List[float]) -> SegmentationInfo:
-        detected_classes_names = [self.class_names[i] for i in detected_classes]
-        seg_info = SegmentationInfo(detected_classes=detected_classes_names,
-                                    scores=scores,
-                                    segmentation=seg_img,
-                                    header=self.header)
-        return seg_info
-
-    def get_label_array(self, detected_classes: List[int]) -> LabelArray:
-        # Label 0 is reserved for the background
-        labels = [Label(id=i + 1, name=self.class_names[i]) for i in detected_classes]
-        lab_arr = LabelArray(header=self.header,
-                             labels=labels)
-        return lab_arr
-
-    def get_score_array(self, scores: List[float]) -> VectorArray:
-        vec_arr = VectorArray(header=self.header,
-                              vector_dim=len(scores),
-                              data=scores)
-        return vec_arr
-
-    def infer(self, msg: Image) -> Tuple[SegmentationInfo, Optional[Image], Optional[Image]]:
-        seg_img, labels, scores, vis_img = self.inference_step(msg)
-        seg_info = self.get_segmentation_info(seg_img, labels, scores)
-
-        if self.node_config.out_debug_img:
-            debug_img = self.get_debug_img(vis_img)
-        else:
-            debug_img = None
-
-        if self.node_config.out_debug_img:
-            debug_seg_img = self.get_debug_segimg()
-        else:
-            debug_seg_img = None
-
-        return seg_info, debug_img, debug_seg_img
+        result = InferenceRawResult(
+            data,
+            class_indexes,
+            scores,
+            visualized_output,
+            msg.header,
+            self.class_names)
+        return result
